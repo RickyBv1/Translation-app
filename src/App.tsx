35f5c927bed338371a32558900813c204c6c5054
RickyBv1 <rickycortes668668@gmail.com>
2025-04-17 12:07:42 -0400

    feat: add state management and integrate Bootstrap for styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,20 @@
-import './App.css'
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
 
 function App() {
 
   return (
-    <>
-    </>
-  )
+    <div className="App">
+      <h1>Translate</h1>
+      <button
+        onClick={() => {
+          dispatch({ type: "SET_FROM_LANGUAGE", payload: "es" });
+        }}
+      >
+        Cambiar a Español
+      </button>
+    </div>
+  );
 }
 
-export default App
+export default App;
